55e2d93c908dba109e8f78e6603461673138d680
natalia <mellomartins.naty@gmail.com>
2021-06-23 20:21:32 -0300

    script aula 4.1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -0,0 +1,42 @@
+console.log("------1------")
+
+let a = 5;
+let b = 10;
+let c = 20;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(b % a);
+
+console.log("------2------")
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+
+console.log("------3------")
+
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
+} else {
+    console.log(c);
+}
+
+console.log("------4------")
+
+if (a>0){
+    console.log("positive");
+} else if (a<0){
+    console.log("negative");
+} else {
+    console.log("zero")
+}
+
+
+
